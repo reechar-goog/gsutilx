ad51fef34e65f76fd5f9a164efea43a8181b0de9
Richard Hsu <reechar@google.com>
2018-11-02 11:01:48 -0400

    initial commit

---
+++ hello.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/tj/go-spin"
+)
+
+func main() {
+	s := spin.New()
+	for i := 0; i < 30; i++ {
+		fmt.Printf("\r  \033[36mcomputing\033[m %s ", s.Next())
+		time.Sleep(100 * time.Millisecond)
+	}
+	fmt.Printf("hello")
+}
